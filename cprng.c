abf8344279c7756fd9ff3492917d2e622ced44ac
Hiroaki Mizuguchi <mhiroaki@gmail.com>
2016-07-17 19:00:06 +0900

    split source for random number generator

---
+++ cprng.c
@@ -0,0 +1,87 @@
+/*
+ *  ssss version 0.5                         -  Copyright 2005,2006 B. Poettering
+ *  ssss version 0.5.1..0.5.4 (changes only) -  Copyright 2011,2013 Jon D. Frisby
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
+ */
+
+/*
+ * http://point-at-infinity.org/ssss/
+ * https://github.com/MrJoy/ssss
+ *
+ * This is an implementation of Shamir's Secret Sharing Scheme. See
+ * the project's homepage http://point-at-infinity.org/ssss/ for more
+ * information on this topic.
+ *
+ * This code links against the GNU multiprecision library "libgmp".
+ * Original author compiled the code successfully with gmp 4.1.4.
+ * Jon Frisby compiled the code successfully with gmp 5.0.2.
+ *
+ * You will need a system that has a /dev/random entropy source.
+ *
+ * Compile with
+ * "gcc -O2 -lgmp -o ssss-split ssss.c && ln ssss-split ssss-combine"
+ *
+ * Compile with -DNOMLOCK to obtain a version without memory locking.
+ *
+ * If you encounter compile issues, compile with USE_RESTORE_SECRET_WORKAROUND.
+ *
+ * Report bugs to: ssss AT point-at-infinity.org
+ * Also report compilation / usability issues to: jfrisby AT mrjoy.com
+ *
+ */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include "cprng.h"
+
+#define RANDOM_SOURCE "/dev/urandom"
+
+extern char *ssss_errmsg;
+
+/* routines for the random number generator */
+
+int cprng_init(void)
+{
+  int cprng = open(RANDOM_SOURCE, O_RDONLY);
+  if (cprng < 0)
+    ssss_errmsg = "couldn't open " RANDOM_SOURCE;
+  return cprng;
+}
+
+int cprng_deinit(int cprng)
+{
+  int ret = close(cprng);
+  if (ret < 0)
+    ssss_errmsg = "couldn't close " RANDOM_SOURCE;
+  return ret;
+}
+
+int cprng_read(int cprng, uint8_t *buf, size_t len)
+{
+  size_t count;
+  int i;
+  for(count = 0; count < len; count += i)
+    if ((i = read(cprng, buf + count, len - count)) < 0) {
+      close(cprng);
+      ssss_errmsg = "couldn't read from " RANDOM_SOURCE;
+      return i;
+    }
+  return 0;
+}
